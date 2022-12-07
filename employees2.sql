b68968f870557914fb07d669d75ff09a9c0f5ec0
Priyanka05hithup <113622249+Priyanka05hithup@users.noreply.github.com>
2022-12-07 17:33:57 -0600

    Adding employees-2 sql file

---
+++ employees2.sql
@@ -0,0 +1,3 @@
+create table employes2(ID integer, EmployeeName varchar(50), BirthDate varchar(20), Notes varchar(100));
+insert into employes2 values(1, 'Rahul Shrama', '1968-12-08', 'Education includes BA in psychology');
+insert into employes2 values(2, 'Fuller Copper', '1952-12-08', 'Education includes BS in psychology');
